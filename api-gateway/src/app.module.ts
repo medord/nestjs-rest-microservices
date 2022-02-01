c0254b56a830f5d5e8e765b8b4aa996690f1fec2
medord <medord@outlook.fr>
2022-02-01 20:47:52 +0100

    added stuff

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
+import { AdressesModule } from './adresses/adresses.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { CashOpsModule } from './cash-ops/cash-ops.module';
+import { CustomersModule } from './customers/customers.module';
+import { HealthCheckModule } from './health-check/health-check.module';
+import { ItemsModule } from './items/items.module';
+import { LoyaltyModule } from './loyalty/loyalty.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { RegistryOpsModule } from './registry-ops/registry-ops.module';
+import { SalesModule } from './sales/sales.module';
+import { ServiceCatalogModule } from './service-catalog/service-catalog.module';
+import { WorkerProcessModule } from './worker-process/worker-process.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AdressesModule,
+    AuthenticationModule,
+    CashOpsModule,
+    CustomersModule,
+    HealthCheckModule,
+    ItemsModule,
+    LoyaltyModule,
+    NotificationsModule,
+    RegistryOpsModule,
+    SalesModule,
+    ServiceCatalogModule,
+    WorkerProcessModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        safe: true,
+        prettyPrint: process.env.NODE_ENV === 'development'
+      }
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { Logger } from 'nestjs-pino'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.useLogger(app.get(Logger))
+  app.enableCors({
+    origin: '*'
+  })
+
+  return app.listen(process.env.PORT)
 }
-bootstrap();
+
+bootstrap()
