7ce61f4020f962ea2890fd95c76d0cd1078db0d2
medord <medord@outlook.fr>
2022-02-15 09:37:26 +0100

    added protos

--- microservices/authentication-svc/src/main.ts
+++ microservices/authentication-svc/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, ClientOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { join } from 'path'
+import * as fs from 'fs'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Logger.log(fs.existsSync(join(__dirname,'../_proto/authentication.proto')))
+  app.connectMicroservice({
+    transport: Transport.GRPC,
+    package: 'auth',
+    protoPath: join(__dirname,'../_proto/authentication.proto'),
+      loader: {
+        enums: String,
+        objects: true,
+        arrays: true
+      }
+  })
+
+  await app.startAllMicroservices();
   await app.listen(3000);
+  Logger.log('Auth microservice running');
 }
 bootstrap();
